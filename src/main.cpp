30149c3ad23065acbb73905a22d17bc9469feb76
Salvador Guzman <guzmansalv@gmail.com>
2021-08-04 16:15:36 -0700

    main stub

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by sguzman on 8/4/21.
+//
+
+#include "main.h"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
+#include <glm/mat4x4.hpp
+#include <glm/vec4.hpp
+#include <iostream>
+
+[[noreturn]] int main() noexcept {
+    glfwInit();
+}
---
+++ src/main.h
@@ -0,0 +1,14 @@
+//
+// Created by sguzman on 8/4/21.
+//
+
+#ifndef VULKAN_EXAMPLE_TEST_MAIN_H
+#define VULKAN_EXAMPLE_TEST_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //VULKAN_EXAMPLE_TEST_MAIN_H
