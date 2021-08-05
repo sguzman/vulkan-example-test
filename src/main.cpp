dea4c843e154e2a2b0e9e572b8a074f7d20c4d0b
Salvador Guzman <guzmansalv@gmail.com>
2021-08-04 17:02:36 -0700

    working example

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,29 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 
-#include <glm/mat4x4.hpp
-#include <glm/vec4.hpp
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
 #include <iostream>
 
 [[noreturn]] int main() noexcept {
     glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Vulkan Window", nullptr, nullptr);
+
+    uint32_t extentionCount{};
+    vkEnumerateInstanceExtensionProperties(nullptr, &extentionCount, nullptr);
+
+    std::cout << extentionCount << " extensions supported" << std::endl;
+
+    glm::mat4 matrix;
+    glm::mat4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
